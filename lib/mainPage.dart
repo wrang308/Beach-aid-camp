55dd3ade6d9c119b44b039493da6c11a62fd0b63
wrang308 <markus.wrang_94@hotmail.com>
2020-01-01 02:45:27 +0100

    changed the look of the main menu buttons

--- lib/mainPage.dart
+++ lib/mainPage.dart
@@ -5,84 +5,105 @@ import 'package:flutter_app_testing_coding/main.dart';
 import 'package:flutter_app_testing_coding/schema.dart';
 import 'package:flutter_app_testing_coding/grouptest.dart';
 import 'drawer.dart';
-
+import 'package:auto_size_text/auto_size_text.dart';
 
 class MainPageRoute extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Main Page Route"),
-        ),
-        drawer: MyDrawer(),
-
-        body: GridView.count(
-    primary: false,
-    padding: const EdgeInsets.all(20),
-    crossAxisSpacing: 10,
-    mainAxisSpacing: 10,
-    crossAxisCount: 2,
-    children: <Widget>[
-      InkWell(
-
-        onTap: () {
+      appBar: AppBar(
+        title: Text("Main Page Route"),
+      ),
+      drawer: MyDrawer(),
+      body: GridView.count(
+          primary: false,
+          padding: const EdgeInsets.all(20),
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+          crossAxisCount: 2,
+          children: <Widget>[
+            InkWell(
+              onTap: () {
+                Navigator.popUntil(context, ModalRoute.withName('/'));
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SchemaRoute()),
+                );
+              }, // handle your onTap here
+              child: Container(
+                height: 200,
+                width: 200,
+                color: Colors.cyan,
+                child: Center(
+                  child: AutoSizeText(
+                    'Schema',
+                    maxLines: 1,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 40,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            InkWell(
+              onTap: () {
+                Navigator.popUntil(context, ModalRoute.withName('/'));
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => GroupRoute()),
+                );
+              }, // handle your onTap here
+              child: Container(
+                height: 200,
+                width: 200,
+                color: Colors.indigo,
+                child: Center(
+                  child: AutoSizeText(
+                    'Grupper',
+                    maxLines: 1,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 40,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ]),
+      /**ListView(children: <Widget>[
+          RaisedButton(
+          onPressed: () {
+          Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => MyHomePage()),
+          );
+          },
+          child: Text('Go back!'),
+          ),
+          RaisedButton(
+          onPressed: () {
           Navigator.popUntil(context, ModalRoute.withName('/'));
           Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => SchemaRoute()),
+          context,
+          MaterialPageRoute(builder: (context) => SchemaRoute()),
           );
-        }, // handle your onTap here
-        child: Container(height: 200, width: 200,
-        color: Colors.cyan,
-        child: Text('Schema'),),
-      ),
-      InkWell(
-
-        onTap: () {
+          },
+          child: Text('Schema'),
+          ),
+          RaisedButton(
+          onPressed: () {
           Navigator.popUntil(context, ModalRoute.withName('/'));
           Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => GroupRoute()),
+          context,
+          MaterialPageRoute(builder: (context) => GroupRoute()),
           );
-        }, // handle your onTap here
-        child: Container(height: 200, width: 200,
-        color: Colors.amber,
-        child: Text('Group'),),
-      ),
-
-    ]),
-        /**ListView(children: <Widget>[
-          RaisedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => MyHomePage()),
-              );
-            },
-            child: Text('Go back!'),
-          ),
-          RaisedButton(
-            onPressed: () {
-              Navigator.popUntil(context, ModalRoute.withName('/'));
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => SchemaRoute()),
-              );
-            },
-            child: Text('Schema'),
-          ),
-          RaisedButton(
-            onPressed: () {
-              Navigator.popUntil(context, ModalRoute.withName('/'));
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => GroupRoute()),
-              );
-            },
-            child: Text('Grupper'),
+          },
+          child: Text('Grupper'),
           ),
 
-        ])
-    **/);
+          ])
+       **/
+    );
   }
 }
