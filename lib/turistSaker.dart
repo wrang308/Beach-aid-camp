7e6ff53154870d65852b5ebb4ce481280edba718
Markus Wrang <markus.wrang_94@hotmail.com>
2020-01-29 01:46:41 +0100

    added new templete for things to see around boda

--- lib/turistSaker.dart
+++ lib/turistSaker.dart
@@ -7,6 +7,7 @@ import 'package:flutter/services.dart';
 import 'groups.dart';
 import 'package:flutter_app_testing_coding/drawer.dart';
 import 'package:page_transition/page_transition.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class TuristRoute extends StatefulWidget {
 
@@ -59,7 +60,7 @@ class _turistState extends State<TuristRoute> {
                 onTap: () {
                   Navigator.push(
                     context,
-                    PageTransition(type: PageTransitionType.fade, child: ByrumPage()f, duration: Duration(milliseconds: 500)),
+                    PageTransition(type: PageTransitionType.fade, child: ByrumPage(), duration: Duration(milliseconds: 500)),
                   );
 
                 }
@@ -162,22 +163,60 @@ class _byrumState extends State<ByrumPage> {
                   stops: [0.1, 0.9],
                   colors: [
                     // Colors are easy thanks to Flutter's Colors class.
-                    Colors.white.withOpacity(0.5),
-                    Colors.blue.withOpacity(0.4),
+                    Colors.white.withOpacity(0.8),
+                    Colors.blue.withOpacity(0.7),
                   ],
                 ),
               ),),
-            ListView(
-              padding: const EdgeInsets.fromLTRB(20,100,20,20),
-              children: <Widget>[
-                ListTile(
-                  leading: CircleAvatar(
-                    backgroundImage: AssetImage("assets/images/byrums-raukar-bla-jungfrun.jpg"), // no matter how big it is, it won't overflow
-                  ),
-                  title: Text('Byrums Raukar'),
+
+              Positioned(
+                top: 100,
+                child: Image(
+                  height: 250,
+
+                  image: AssetImage("assets/images/byrums-raukar-bla-jungfrun.jpg"),
                 ),
-              ],
+              ),
+
+            Positioned(
+                top: 350,
+                width: MediaQuery.of(context).size.width,
+                child: Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Text("Ölands enda raukar bara ett stenkast från Böda, åk"
+                    " dit och njut av när solen går ned över Blå jungfrun. ",
+                    style: TextStyle(
+                      fontSize: 20,
+
+                    ),),)
             ),
+
+
+            Align(
+              alignment: Alignment(0.0, 0.95),
+
+              child: InkWell(
+                  splashColor: Colors.blue.withAlpha(30),
+                  child: Card(
+
+                  child: Padding(
+                    padding: EdgeInsets.all(16.0),
+
+                child:Text("hitta hit",
+                style: TextStyle(
+                    fontSize: 20,
+
+                ),
+                    textAlign: TextAlign.center),
+            ),
+                ),
+                onTap: (){
+                launchURL('https://www.google.se/maps/place/B%C3%B6dag%C3%A5rdens+Camping/@57.2586348,17.0482654,17z/data=!3m1!4b1!4m8!3m7!1s0x46f7fee2c478294d:0xddb1b62af6d8274c!5m2!4m1!1i2!8m2!3d57.2586319!4d17.0504541');
+                }
+
+            ),
+            ),
+
             new Positioned(
               //Place it at the top, and not use the entire screen
               top: 0.0,
@@ -195,5 +234,14 @@ class _byrumState extends State<ByrumPage> {
               ),
             ),],)
     );
+  }
+}
+
+launchURL(url) async {
+  //const url = 'https://www.google.se/maps/place/B%C3%B6dag%C3%A5rdens+Camping/@57.2586348,17.0482654,17z/data=!3m1!4b1!4m8!3m7!1s0x46f7fee2c478294d:0xddb1b62af6d8274c!5m2!4m1!1i2!8m2!3d57.2586319!4d17.0504541';
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
   }
 }
